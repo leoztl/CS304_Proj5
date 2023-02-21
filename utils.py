450415a3f78cbec0478a4f8a126c0b3d57c021f5
Tianle Zhu <leoztl@outlook.com>
2023-02-21 23:40:30 +0800

    utils

---
+++ utils.py
@@ -0,0 +1,60 @@
+import pickle
+import queue
+
+
+class HMM:
+    def __init__(self, state_ls, trans_mat, name):
+        self.state_ls = state_ls
+        self.trans_mat = trans_mat
+        self.name = name
+
+
+def save_hmm(hmm):
+    filename = "./model/" + hmm.name + ".pickle"
+    with open(filename, "wb") as file:
+        pickle.dump(hmm, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def load_hmm(digit):
+    filename = "./model/" + digit + ".pickle"
+    with open(filename, "rb") as file:
+        return pickle.load(file)
+
+
+def dfs_print(head):
+    # depth first print tree
+    if head == None:
+        return
+    head.visited = True
+    print(head.id)
+    for child in head.next:
+        if not child.visited:
+            dfs_print(child)
+
+
+def countNode(head):
+    if head == None:
+        return 0
+    else:
+        count = 0
+        for child in head.next:
+            if child.visited:
+                continue
+            child.visited = True
+            count += countNode(child)
+        return 1 + count
+
+
+def initialize(head):
+    id = 0
+    head.visited = False
+    q = queue.Queue()
+    q.put(head)
+    while not q.empty():
+        node = q.get()
+        node.id = id
+        id += 1
+        for child in node.next:
+            if child.visited:
+                child.visited = False
+                q.put(child)
